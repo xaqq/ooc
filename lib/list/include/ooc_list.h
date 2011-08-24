f8c3c85695055874bfd65c72e7a3b0e97a49cd8b
Kapp Arnaud <kapp.arno@gmail.com>
2011-08-24 17:49:29 +0200

    Code for list's lib.

--- lib/list/include/ooc_list.h
+++ lib/list/include/ooc_list.h
@@ -1,6 +1,8 @@
 #ifndef		__OOC_LIST_H__
 #define		__OOC_LIST_H__
 
+#define		list_append(x, y)	list_append_real_function((x), ((void *)&(y)));
+
 typedef struct		s_list_item
 {
   unsigned char		*data;
@@ -10,13 +12,15 @@ typedef struct		s_list_item
 typedef struct	s_list
 {
   void		*_class;
+  int		data_size;
   t_list_item	*first;
-  t_list_item	*cur;  
+  t_list_item	*last;
 }		t_list;
 
 void		*list_ctor(void *_self, va_list *ap);
 void		*list_dtor(void *_self);
-
+void		list_append_real_function(void *_self, void *data);
+void		*list_get_last(void *_self);
 extern const void *List;
 
 #endif
---
+++ lib/list/src/list.c
@@ -0,0 +1,55 @@
+#include	<stdarg.h>
+#include	<stdlib.h>
+#include	"ooc_base.h"
+#include	"ooc_list.h"
+
+void		*list_ctor(void *_self, va_list *ap)
+{
+  t_list	*self = (t_list *)_self;
+  int		size = va_arg(*ap, int); /* size of data saved in list */
+
+  self->data_size = size;
+  self->first = NULL;
+  self->last = NULL;
+  return (_self);  
+}
+
+void		*list_dtor(void *_self)
+{
+  t_list	*self = (t_list *)_self;
+  
+  self = self;
+  return (_self);
+}
+
+void		list_append_real_function(void *_self, void *data)
+{
+  t_list	*self = (t_list *)_self;
+  t_list_item	*item = malloc(sizeof(t_list_item));
+  unsigned char	*storage;
+  int		i;
+
+  storage = malloc(self->data_size);
+  memcpy(storage, *(void **)data, self->data_size);
+  item->data = data;
+  self->last = item;
+}
+
+void		*list_get_last(void *_self)
+{
+  t_list	*self = (t_list *)_self;
+  return ((void *)(self->last->data));
+}
+
+
+
+static const t_class _List = 
+  {
+    sizeof(struct s_list),
+    &list_ctor,
+    &list_dtor,
+    NULL
+  };
+
+
+const void *List = &_List;
